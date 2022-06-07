fa3acac4afb7bf7012d229d2d52c3c918adc5041
hdwang <1182583214@qq.com>
2022-06-07 20:37:20 +0800

    字符串转数组、列表等也改用JSONUtil.toBean

--- openapi-sdk-common/src/main/java/openapi/sdk/common/util/StrObjectConvert.java
+++ openapi-sdk-common/src/main/java/openapi/sdk/common/util/StrObjectConvert.java
@@ -4,13 +4,8 @@ import cn.hutool.core.convert.Convert;
 import cn.hutool.core.util.ClassUtil;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.json.JSONUtil;
-import openapi.sdk.common.model.BusinessException;
 
-import java.lang.reflect.Array;
-import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
-import java.util.Collection;
-import java.util.List;
 
 /**
  * 字符串对象转换工具类
@@ -29,12 +24,8 @@ public class StrObjectConvert {
     public static Object strToObj(String str, Type type) {
         String classTypeName = type.getTypeName();
         boolean isClassType = false;
-        boolean isParameterizedType = false;
         if (type instanceof Class) {
             isClassType = true;
-        }
-        if (type instanceof ParameterizedType) {
-            isParameterizedType = true;
         }
         Object ins = null;
         if (isClassType && ClassUtil.isBasicType((Class) type)) {
@@ -48,36 +39,8 @@ public class StrObjectConvert {
             }
         } else if (classTypeName.equals(String.class.getName())) {
             ins = str;
-        } else if (isClassType && ((Class) type).isArray()) {
-            Class elementType = ((Class) type).getComponentType();
-            //数组类型先转成List
-            List list = JSONUtil.toList(str, elementType);
-            //构建一个数组对象，然后把List数据赋值给数组
-            Object array = Array.newInstance(elementType, list.size());
-            for (int i = 0; i < list.size(); i++) {
-                Array.set(array, i, list.get(i));
-            }
-            ins = array;
-        } else if (isParameterizedType) {
-            ParameterizedType parameterizedType = (ParameterizedType) type;
-            Class rawType = (Class) parameterizedType.getRawType();
-            if (Collection.class.isAssignableFrom(rawType)) {
-                if (rawType.getName().equals(List.class.getName())) {
-                    //Collection类型都转换为List(注：不支持set等、也不支持List里套List)
-                    Type componentType = parameterizedType.getActualTypeArguments()[0];
-                    if (componentType instanceof Class) {
-                        ins = JSONUtil.toList(str, (Class) componentType);
-                    } else {
-                        throw new BusinessException("不支持泛型类里套泛型的参数");
-                    }
-                } else {
-                    throw new BusinessException("不支持除List以外的集合类型参数");
-                }
-            } else {
-                throw new BusinessException("不支持除List以外的参数化类型参数");
-            }
         } else {
-            //对象转换
+            //对象转换：可以将JSON字符串直接转换为任意对象（Bean、Map、集合、数组等）
             ins = JSONUtil.toBean(str, type, false);
         }
         return ins;
@@ -99,7 +62,7 @@ public class StrObjectConvert {
             //字符串类型，无需转换
             str = (String) obj;
         } else {
-            //对象转换
+            //对象转换：可以将任意对象（Bean、Map、集合、数组等）直接转换为JSON字符串
             str = JSONUtil.toJsonStr(obj);
         }
         return str;
