0efb7873a99f402905d2b6fc427f00ace51a285e
hdwang <1182583214@qq.com>
2022-06-09 23:56:21 +0800

    新增拼接网址工具类

--- openapi-sdk-common/src/main/java/openapi/sdk/common/util/CommonUtil.java
+++ openapi-sdk-common/src/main/java/openapi/sdk/common/util/CommonUtil.java
@@ -1,6 +1,7 @@
 package openapi.sdk.common.util;
 
 import cn.hutool.core.util.StrUtil;
+import openapi.sdk.common.model.BusinessException;
 import openapi.sdk.common.model.InParams;
 
 /**
@@ -21,5 +22,27 @@ public class CommonUtil {
         //使用数据+uuid作为签名的内容，保证无参函数调用也能经过签名的验证
         String body = StrUtil.isBlank(inParams.getBody()) ? StrUtil.EMPTY : inParams.getBody();
         return body + inParams.getUuid();
+    }
+
+    /**
+     * 拼接url地址
+     *
+     * @param baseUrl 基础路径
+     * @param path    待拼接的路径
+     * @return 完整的url地址
+     */
+    public static String completeUrl(String baseUrl, String path) {
+        if (StrUtil.isBlank(baseUrl)) {
+            throw new BusinessException("URL基础路径不能为空");
+        }
+        String separator = "/";
+        if (baseUrl.endsWith(separator)) {
+            baseUrl = baseUrl.substring(0, baseUrl.length() - 1);
+        }
+        if (StrUtil.isNotBlank(path)) {
+            path = path.startsWith(separator) ? path.substring(1) : path;
+            return baseUrl + separator + path;
+        }
+        return baseUrl;
     }
 }
