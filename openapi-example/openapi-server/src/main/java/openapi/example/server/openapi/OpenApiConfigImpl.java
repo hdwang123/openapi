1bcc78dddb4b1fb06a064189def2f42e4b197370
hdwang <1182583214@qq.com>
2022-07-18 20:47:09 +0800

    修改案例代码

--- openapi-example/openapi-server/src/main/java/openapi/example/server/openapi/OpenApiConfigImpl.java
+++ openapi-example/openapi-server/src/main/java/openapi/example/server/openapi/OpenApiConfigImpl.java
@@ -65,7 +65,7 @@ public class OpenApiConfigImpl implements OpenApiConfig {
 
     @Override
     public boolean enableCompress() {
-        //禁用压缩
-        return false;
+        //HTTP传输内容启用压缩
+        return true;
     }
 }
